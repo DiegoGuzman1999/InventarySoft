6f3331afe83469e358e622c93627fc89dfceb6b5
Diego Penagos <diegopenagos984@gmail.com>
2026-03-23 14:59:28 -0500

    docs(readme): redefinir alcance del repo como docs-first

--- inventory-service/src/main/java/com/inventarysoft/inventoryservice/shared/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.inventarysoft.inventoryservice.shared.exception;
-
-public class ConflictException extends RuntimeException {
-  public ConflictException(String message) {
-    super(message);
-  }
-}
-
